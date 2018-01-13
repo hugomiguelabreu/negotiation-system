ce29555cfebfdd180003619e73b237fcf9e6b658
Hugo Abreu <hugo.miguel.abreu@gmail.com>
2018-01-13 12:03:18 +0000

    Adicionados as classes para o serviço REST

--- client/src/main/java/rest/RESTClient.java
+++ client/src/main/java/rest/RESTClient.java
@@ -1,9 +1,8 @@
 package rest;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import rest.core.Company;
+import rest.core.Exchange;
+import rest.core.PriceInfo;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -11,34 +10,59 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 
 public class RESTClient {
 
-    Client client = ClientBuilder.newClient();
+    HashMap<String, Company> cache;
+    Client client;
     WebTarget webTarget;
     Invocation.Builder invocationBuilder;
     Response response;
 
     public RESTClient(){
+        client = ClientBuilder.newClient();
+        cache = new HashMap<>();
+    }
 
-        webTarget = client.target("http://localhost:8080").path("company/MCS");
+    public Company getCompany(String id){
+
+        if(cache.containsKey(id))
+            return cache.get(id);
+
+        webTarget = client.target("http://localhost:8080").path("company/" + id);
         invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
         response = invocationBuilder.get();
 
+        return response.readEntity(Company.class);
+    }
 
-        Company msc = response.readEntity(Company.class);
-        System.out.println(msc.getName());
-        System.out.println(msc.getExchange().getAddr());
-
+    public Company[] getCompanies(){
         webTarget = client.target("http://localhost:8080").path("companies");
         invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
         response = invocationBuilder.get();
+        Company[] cs = response.readEntity(Company[].class);
 
-        Company[] kek = response.readEntity(Company[].class);
+        for (Company aux: cs)
+            cache.put(aux.getId(), aux);
 
-        for(Company x: kek){
-            System.out.println(x.getName());
-        }
+        return cs;
+    }
+
+    public Exchange[] getExchanges(){
+        webTarget = client.target("http://localhost:8080").path("exchanges");
+        invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+        response = invocationBuilder.get();
+
+        return response.readEntity(Exchange[].class);
+    }
+
+    public PriceInfo getPrice(String company, int i){
+        webTarget = client.target("http://localhost:8080").path("company/" + company + "/" + (i == 0 ? "today" : "yesterday"));
+        invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+        response = invocationBuilder.get();
+
+        return response.readEntity(PriceInfo.class);
     }
 
 }
